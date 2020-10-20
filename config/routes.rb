428c49bbafbb73675b49ef4ad0fd5fad98073506
Blaise <bboumdakuissi@yahoo.fr>
2020-10-20 18:33:01 +0100

    Adding the post route for the form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index', as: 'tasks'
 
   get 'tasks/:id', to: 'tasks#show', as: 'task', id: /\d+/
+
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+
+  post 'tasks', to: 'tasks#create'
+
 end
